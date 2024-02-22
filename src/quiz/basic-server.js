b55778a03d835122dde008adca905479c21d8a92
Shreya-Nelakonda <nelakonda.s@northeastern.edu>
2024-02-22 17:02:38 -0500

    02222024

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -46,7 +46,12 @@ const server = http.createServer((req, res) => {
                 return incident.id === `MABOS00${id}`
             })));
         }
-    } else {
+        else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Invalid parameters');
+        }
+    }
+    else {
         // If the request is for an unsupported endpoint, send a not found response
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Endpoint not found');
